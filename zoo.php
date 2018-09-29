b9bd64aa1b18c9a895eda74fa4e77cc92643826e
raikeeen <sonnikcs@mail.ru>
2018-09-29 07:28:48 +0500

    pdasdasd

--- zoo.php
+++ zoo.php
@@ -5,6 +5,7 @@
  * Date: 26.09.2018
  * Time: 19:05
  */
+
 class zoo{
     public $animal;
     public $arrayAll=array();
@@ -12,8 +13,8 @@ class zoo{
         $this -> animal= $animal;
         $this->arrayAll= $arrayAll;
     }
-    function add($animal,$arrayAll){
-       return $arrayAll.array_push($animal);
+   public function add($animal){
+       return $this -> arrayAll.array_push($animal);
     }
     function view(){
         foreach ($this->arrayAll as $a){
@@ -21,70 +22,105 @@ class zoo{
         }
     }
 }
-class cell{
+class cell {
     public $zam;
     public $cell;
     function  __construct($zam,$cell){
         $this -> zam= $zam;
         $this->cell= $cell;
     }
-    function zam($zam){
+    function cell($cell,$zam){
         if($zam===true)
-            echo "Open";
-        else echo "Close";
-    }
-    function cell($cell){
-        if($cell===true)
-            echo "Open";
-        else echo "Close";
+            if($cell===true)
+                echo "Open cell and zam";
+            else echo "Close cell, open zam";
+        else echo "Close cell and zam";
     }
 }
-class animal{
+class animal   {
     public $name;
     public $old;
     public $sex;
-    function  __construct($name,$old,$sex){
+    public $cell;
+    function  __construct($name,$old,$sex,$cell){
         $this -> name= $name;
         $this->old= $old;
         $this->sex= $sex;
+        $this->cell= $cell;
     }
 }
-class mammals extends animal {
-    function  __construct($name,$old,$sex){
-        $this -> name= $name;
-        $this->old= $old;
-        $this->sex= $sex;
+class mammals extends animal implements actions{
+    public $feet;
+    public $tail;
+    function  __construct($name,$old,$sex,$cell,$feet,$tail){
+        $this->feet= $feet;
+        $this->tail= $tail;
+        parent::__construct($name,$old,$sex,$cell);
+
+    }
+    public function action(){
+
     }
 
 }
-class bird extends animal {
-    function  __construct($name,$old,$sex){
-        $this -> name= $name;
-        $this->old= $old;
-        $this->sex= $sex;
-    }
-}
-class fish extends animal {
-    function  __construct($name,$old,$sex){
-        $this -> name= $name;
-        $this->old= $old;
-        $this->sex= $sex;
-    }
-}
-class amphibians extends animal {
-    function  __construct($name,$old,$sex){
-        $this -> name= $name;
-        $this->old= $old;
-        $this->sex= $sex;
-    }
-}
-class reptiles extends animal {
-    function  __construct($name,$old,$sex){
-        $this -> name= $name;
-        $this->old= $old;
-        $this->sex= $sex;
-    }
-}
+class bird extends animal implements actions{
+    public $feet;
+    function  __construct($name,$old,$sex,$cell,$feet){
 
+        $this->feet= $feet;
+        parent::__construct($name,$old,$sex,$cell);
+    }
+    public function action(){
+
+    }
+}
+class fish extends animal implements actions {
+    public $feet;
+    function  __construct($name,$old,$sex,$cell,$feet){
+
+        $this->feet= $feet;
+        parent::__construct($name,$old,$sex,$cell);
+    }
+    public function action(){
+
+    }
+}
+class amphibians extends animal implements actions {
+    public $feet;
+    function  __construct($name,$old,$sex,$cell,$feet){
+
+        $this->feet= $feet;
+        parent::__construct($name,$old,$sex,$cell);
+    }
+    public function action(){
+
+    }
+}
+class reptiles extends animal implements actions {
+    public $feet;
+    function  __construct($name,$old,$sex,$cell,$feet){
+
+        $this->feet= $feet;
+        parent::__construct($name,$old,$sex,$cell);
+    }
+    public function action(){
+
+    }
+
+}
+interface actions{
+    public function action();
+}
+$z=new zoo();
+$a=new animal("kotya",12,"male",true);
+$z.add($a);
+//$sobaka=new mammals("yrod",14,"male",true,4,true);
+//$kowka=new mammals("may",4,"male",true,4,true);
+//$kowka=new animal("myka",4,"female",cell.$cell(true));
+//$olen=new animal("myka",4,"female",cell.$cell(true));
+//$zoo=new zoo();
+//$zoo.add($sobaka);
+//$zoo.add($kowka);
+//$zoo.view();
 
 
